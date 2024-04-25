92a513177c096fb203320e3af3b6a4d035292b56
Roshan Weerawardhana <RoshanKW_HO@ntplc.int>
2024-04-25 10:28:56 +0530

    Changes made  in admin entity class

---
+++ src/main/java/com/example/BusinessProject/dto/Admin.java
@@ -0,0 +1,48 @@
+package com.example.BusinessProject.dto;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="admin")
+public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String adminusername;
+    private String adminpassword;
+
+    public Admin() {
+    }
+
+    public Admin(int id, String adminusername, String adminpassword) {
+        this.id = id;
+        this.adminusername = adminusername;
+        this.adminpassword = adminpassword;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAdminusername() {
+        return adminusername;
+    }
+
+    public void setAdminusername(String adminusername) {
+        this.adminusername = adminusername;
+    }
+
+    public String getAdminpassword() {
+        return adminpassword;
+    }
+
+    public void setAdminpassword(String adminpassword) {
+        this.adminpassword = adminpassword;
+    }
+}
